ab9f5d8315fa851338f06e49b6d651005c37b37e
IgorKondratyuk <igorkondratyuk3@gmail.com>
2023-03-26 22:05:51 +0300

    fixed post validation

--- src/modules/blogs-composition/modules/posts/dto/create-post-of-blog.dto.ts
+++ src/modules/blogs-composition/modules/posts/dto/create-post-of-blog.dto.ts
@@ -1,26 +1,24 @@
 import { IsNotEmpty, IsOptional, IsString, MaxLength, MinLength, Validate } from 'class-validator';
 import { Transform } from 'class-transformer';
-import { UserExistsRule } from '../../../../users/validators/user-exists.validator';
-import { BlogExistsRule } from '../../blogs/validators/blog-exists.validator';
 
 export class CreatePostOfBlogDto {
-  @IsNotEmpty()
   @IsString()
   @Transform(({ value }) => value?.trim())
+  @IsNotEmpty()
   @MinLength(1)
   @MaxLength(30)
   title: string;
 
-  @IsNotEmpty()
   @IsString()
   @Transform(({ value }) => value?.trim())
+  @IsNotEmpty()
   @MinLength(1)
   @MaxLength(100)
   shortDescription: string;
 
-  @IsNotEmpty()
   @IsString()
   @Transform(({ value }) => value?.trim())
+  @IsNotEmpty()
   @MinLength(1)
   @MaxLength(1000)
   content: string;
